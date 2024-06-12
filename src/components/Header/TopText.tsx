d29fb767637b2daec12711963d9f08f0253c376f
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-06-12 00:27:55 -0300

    chore: Update metadata and viewport configuration

--- src/components/Header/TopText.tsx
+++ src/components/Header/TopText.tsx
@@ -1,7 +1,7 @@
 export default function TopText({ text }: { text: string }) {
   return (
     <div className="bg-grey_pebble px-2 py-1 text-center font-dm_mono text-[9px] font-thin text-white sm:text-sm">
-      <p className="animate-pulse animate-normal animate-duration-[5000ms] animate-fill-both animate-infinite animate-ease-in-out">
+      <p className="animate-pulse animate-normal animate-duration-[3000ms] animate-fill-both animate-once animate-ease-in-out">
         {text}
       </p>
     </div>
