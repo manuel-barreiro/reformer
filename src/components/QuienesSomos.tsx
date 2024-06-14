1f2082ff1ae0c42cccfe1e4ee805082ceb8f0b3c
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-06-14 12:11:36 -0300

    chore: Optimize image loading with lazy loading and parallax effect

--- src/components/QuienesSomos.tsx
+++ src/components/QuienesSomos.tsx
@@ -1,17 +1,28 @@
+"use client"
 import { quienes_somos, cuerpo, bienestar, espiritu, mente } from "../../public"
 import Image from "next/image"
+import { useRef } from "react"
+import { motion, useScroll, useTransform } from "framer-motion"
 
 export default function QuienesSomos() {
+  const sectionRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  })
+  const y = useTransform(scrollYProgress, [0, 1], ["-20%", "10%"])
   return (
-    <section className="flex h-auto w-full flex-col lg:h-[100vh] lg:flex-row">
+    <section className="flex h-auto w-full flex-col overflow-hidden lg:h-[100vh] lg:flex-row">
       <div className="relative h-screen lg:block lg:basis-[40%]">
-        <Image
-          alt="Reformer Wellness Club"
-          title="Reformer"
-          priority={true}
-          className="absolute inset-0 h-full w-full object-cover object-center"
-          src={quienes_somos}
-        />
+        <motion.div className="absolute h-[120%] w-full" style={{ top: y }}>
+          <Image
+            alt="Reformer Wellness Club"
+            title="Reformer"
+            priority={true}
+            className="inset-0 h-full w-full object-cover object-center"
+            src={quienes_somos}
+          />
+        </motion.div>
       </div>
 
       <div className="flex h-full flex-col items-center justify-center bg-pearl px-14 py-28 lg:basis-[60%] lg:px-20">
