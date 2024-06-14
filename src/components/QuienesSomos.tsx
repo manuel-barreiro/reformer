0d5657027625d31c8812782beae6e20a1681f052
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-06-14 13:32:00 -0300

    aa

--- src/components/QuienesSomos.tsx
+++ src/components/QuienesSomos.tsx
@@ -26,7 +26,7 @@ export default function QuienesSomos() {
       </div>
 
       <div className="flex h-auto flex-col items-center justify-center bg-pearl px-14 py-20 lg:basis-[60%] lg:px-20 lg:py-28">
-        <div className="flex max-w-lg flex-col items-start gap-9 text-justify font-dm_sans">
+        <div className="flex max-w-lg flex-col items-start gap-10 text-justify font-dm_sans">
           <p className="font-dm_mono text-midnight">QUIENES SOMOS</p>
           <p>
             Hacemos de tu bienestar nuestra prioridad. Reformer Wellness Club
@@ -43,23 +43,23 @@ export default function QuienesSomos() {
             clases con el propósito de que logres una transformación tanto
             externa como interna. Buscamos que logres esa plenitud integral.
           </p>
-          <div className="grid h-auto w-full grid-cols-2 gap-10 sm:flex sm:h-20 sm:items-center sm:justify-between">
-            <div className="flex h-full flex-col items-center gap-4">
-              <Image src={cuerpo} alt="cuerpo" className="h-4/5" />
+          <div className="grid h-auto w-full grid-cols-2 gap-8 sm:flex sm:items-center sm:justify-between">
+            <div className="flex h-32 flex-col items-center justify-center gap-4">
+              <Image src={cuerpo} alt="cuerpo" className="h-3/5" />
               <span className="font-dm_mono text-xs text-midnight">CUERPO</span>
             </div>
-            <div className="flex h-full flex-col items-center gap-4">
-              <Image src={mente} alt="mente" className="h-4/5" />
+            <div className="flex h-32 flex-col items-center justify-center gap-4">
+              <Image src={mente} alt="mente" className="h-3/5" />
               <span className="font-dm_mono text-xs text-midnight">MENTE</span>
             </div>
-            <div className="flex h-full flex-col items-center gap-4">
-              <Image src={espiritu} alt="espiritu" className="h-4/5" />
+            <div className="flex h-32 flex-col items-center justify-center gap-4">
+              <Image src={espiritu} alt="espiritu" className="h-3/5" />
               <span className="font-dm_mono text-xs text-midnight">
                 ESPÍRITU
               </span>
             </div>
-            <div className="flex h-full flex-col items-center gap-4">
-              <Image src={bienestar} alt="bienestar" className="h-4/5" />
+            <div className="flex h-32 flex-col items-center justify-center gap-4">
+              <Image src={bienestar} alt="bienestar" className="h-3/5" />
               <span className="font-dm_mono text-xs text-midnight">
                 BIENESTAR
               </span>
