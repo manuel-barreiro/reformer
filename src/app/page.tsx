eaca08e852829cb901b46f57027bada01bda3ef6
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-06-14 11:21:11 -0300

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client"
 import Brand from "@/components/Brand/Brand"
 import ComingSoon from "@/components/ComingSoon"
 import Header from "@/components/Header/Header"
 import Hero from "@/components/Hero/Hero"
 import QuienesSomos from "@/components/QuienesSomos"
+import { useEffect } from "react"
+import Lenis from "lenis"
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    lenis.on("scroll", (e: any) => {
+      console.log(e)
+    })
+
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
   return (
     <main className="">
       <Header />
