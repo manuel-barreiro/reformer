b7c1d99ef63fc1c62d98bb1722b6b0035ba61269
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-06-14 12:04:19 -0300

    parallax

--- src/components/Brand/BrandBottom.tsx
+++ src/components/Brand/BrandBottom.tsx
@@ -1,17 +1,28 @@
+"use client"
 import React from "react"
 import { brand } from "../../../public"
 import Image from "next/image"
+import { useRef } from "react"
+import { motion, useScroll, useTransform } from "framer-motion"
 
 function BrandBottom() {
+  const sectionRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  })
+  const y = useTransform(scrollYProgress, [0, 1], ["-40%", "20%"])
   return (
-    <div className="relative h-auto w-full">
-      <Image
-        alt="Reformer Wellness Club"
-        title="Reformer"
-        priority={true}
-        className="absolute inset-0 h-full w-full object-cover object-center"
-        src={brand}
-      />
+    <div className="relative h-auto w-full overflow-hidden">
+      <motion.div className="absolute h-[140%] w-full" style={{ top: y }}>
+        <Image
+          alt="Reformer Wellness Club"
+          title="Reformer"
+          priority={true}
+          className="inset-0 h-full w-full object-cover object-left"
+          src={brand}
+        />
+      </motion.div>
       <div className="justify-center-center relative flex h-full w-full">
         <div className="flex h-full flex-col items-start gap-8 p-14 sm:p-28 lg:ml-[20rem] xl:ml-[24rem]">
           <h2 className="font-marcellus text-4xl text-pearl sm:text-5xl md:text-5xl">
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,17 +1,34 @@
+"use client"
+
 import { hero } from "../../../public"
 import Image from "next/image"
 import HeroText from "./HeroText"
+import { useRef } from "react"
+import { motion, useScroll, useTransform } from "framer-motion"
 
 export default function Hero() {
+  const sectionRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  })
+  const y = useTransform(scrollYProgress, [0, 1], ["-20%", "10%"])
+
   return (
-    <section className="relative h-[80vh] w-full md:h-[90vh]">
-      <Image
-        alt="Reformer Wellness Club"
-        title="Reformer"
-        priority={true}
-        className="absolute inset-0 h-full w-full object-cover object-left"
-        src={hero}
-      />
+    <section
+      ref={sectionRef}
+      className="relative h-[80vh] w-full overflow-hidden md:h-[90vh]"
+    >
+      <motion.div className="absolute h-[120%] w-full" style={{ top: y }}>
+        <Image
+          alt="Reformer Wellness Club"
+          title="Reformer"
+          priority={true}
+          className="inset-0 h-full w-full object-cover object-left"
+          src={hero}
+        />
+      </motion.div>
+
       <div className="relative flex h-full w-full justify-start px-8 sm:justify-end lg:px-16">
         <HeroText />
       </div>
