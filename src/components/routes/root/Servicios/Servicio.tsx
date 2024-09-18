7ac30be4ae07e8b9d874124a172f74d192487504
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-09-18 11:30:57 -0300

    types import fix

--- src/components/routes/root/Servicios/Servicio.tsx
+++ src/components/routes/root/Servicios/Servicio.tsx
@@ -1,6 +1,6 @@
 import { StaticImageData } from "next/image"
 import Image from "next/image"
-import { ServicioInfo } from "../../../types"
+import { ServicioInfo } from "@/lib/types"
 
 type ServicioProps = {
   img: StaticImageData
