fe035e78602a35de8403447922b1108442ad7728
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-10-15 17:35:48 -0300

    package api rout nextrequest correction

--- src/app/api/(packages)/admin-packages/route.ts
+++ src/app/api/(packages)/admin-packages/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { getAllClassPackages } from "@/actions/package-actions"
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const packages = await getAllClassPackages()
     return NextResponse.json(packages)
--- src/app/api/(packages)/user-packages/route.ts
+++ src/app/api/(packages)/user-packages/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { getActiveClassPackages } from "@/actions/package-actions"
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const packages = await getActiveClassPackages()
     return NextResponse.json(packages)
