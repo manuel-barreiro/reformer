711083fc6c895a9e5e29f8335d5030e7e63417b4
Manuel <manuel.barreiro@ing.austral.edu.ar>
2024-11-11 10:23:22 -0300

    improvements class card

--- src/components/modules/roles/common/calendario/ClassCard.tsx
+++ src/components/modules/roles/common/calendario/ClassCard.tsx
@@ -179,7 +179,9 @@ export default function ClassCard({
       <div className="relative flex w-full flex-col items-start justify-between gap-3 2xl:flex-row 2xl:items-center 2xl:gap-6">
         <div className="w-auto">
           <p className="flex items-center gap-2 text-sm">
-            <Users className="h-4 w-4 text-midnight" />
+            <Users
+              className={`h-4 w-4 ${userRole === "user" && userBooking ? "text-pearl" : "text-midnight"}`}
+            />
             <span>
               {confirmedBookings.length}/{class_.maxCapacity}
             </span>
@@ -409,10 +411,12 @@ export default function ClassCard({
           </div>
         )}
       </div>
-      <Progress
-        value={(confirmedBookings.length / class_.maxCapacity) * 100}
-        className="absolute left-0 right-0 top-0 h-1 w-full rounded-b-none rounded-t-md"
-      />
+      {userRole === "admin" && (
+        <Progress
+          value={(confirmedBookings.length / class_.maxCapacity) * 100}
+          className="absolute left-0 right-0 top-0 h-1 w-full rounded-b-none rounded-t-md"
+        />
+      )}
     </Card>
   )
 }
